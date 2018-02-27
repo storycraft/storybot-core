3434d41086ebb47496064047356e7414db8fda78
storycraft <dbsxoal123@naver.com>
2018-02-28 01:11:14 +0900

    add network helper

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import StoryBot from './bot';
 import MessageTemplate, { MessageAttachment } from './message/template/message-template';
 import CommandListener from './command/command-listener';
+import RequestHelper from './io/network/request-helper';
 
-export { StoryBot, MessageTemplate, MessageAttachment, CommandListener }
+export { StoryBot, MessageTemplate, MessageAttachment, CommandListener, RequestHelper }
---
+++ src/io/network/request-helper.js
@@ -0,0 +1,35 @@
+import http from 'http';
+import https from 'https';
+
+import { URL } from 'url';
+
+export default class RequestHelper {
+    static async get(requestUrl){
+        return await RequestHelper.request(new URL(requestUrl), null, RequestHelper.isHttps(requestUrl));
+    }
+
+    static async post(requestUrl, data, isHttps){
+        return await RequestHelper.request(new URL(requestUrl), data, RequestHelper.isHttps(requestUrl));
+    }
+
+    static async request(option, data, isHttps){
+        var module = isHttps ? https : http;
+
+        return await new Promise((resolve, reject) => {
+            var req = module.request(option,(res) => {
+                var data = '';
+                res.on('data', (chunk) => data += chunk);
+    
+                res.on('end', () => resolve(data));
+            });
+
+            if (data)
+                req.write(data);
+            req.end();
+        });
+    }
+
+    static isHttps(requestUrl){
+        return requestUrl.startsWith('https');
+    }
+}
