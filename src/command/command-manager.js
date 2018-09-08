07e86a8cdaea4a8957e525c7c1e2a437e6ee8876
storycraft <dbsxoal123@naver.com>
2018-09-08 19:24:39 +0900

    fix command spliting

--- src/command/command-manager.js
+++ src/command/command-manager.js
@@ -7,6 +7,7 @@ export default class CommandManager extends EventEmitter {
         this.bot = bot;
         
         this.commandPrefix = '*';
+        this.spliter = ' ';
 
         this.commandInfoList = [];
 
@@ -29,16 +30,21 @@ export default class CommandManager extends EventEmitter {
         if (!msg.Text.startsWith(this.CommandPrefix))
             return;
 
-        var commandIndex = msg.Text.indexOf(' ');
+        var commandIndex = msg.Text.indexOf(this.spliter);
 
         if (commandIndex == -1)
             commandIndex = msg.Text.length;
     
         var command = msg.Text.substring(this.CommandPrefix.length, commandIndex);
-        var args = msg.Text.substring(this.CommandPrefix.length + commandIndex).split(' ');
-
         if (command == '')
             return;
+
+        var rawArgument = msg.Text.substring(commandIndex + this.spliter.length);
+        var args = [];
+        
+        if (rawArgument != '') {
+            args = rawArgument.split(' ');
+        }
 
         //WTF best idea ever
         this.emit(command, args, msg.User, this.Bot, msg.Source);
